ad04134239d6f59a0178c9ccab9f4a8f45c9b26f
Shlain08 <109956769+Shlain08@users.noreply.github.com>
2022-07-26 16:33:45 +0530

    updated file

---
+++ Assignment.java
@@ -0,0 +1,116 @@
+//QUESTION 1
+
+class Assignment
+{
+public static void main(String[] args)
+{   Assignment obj3 = new Assignment();
+        obj3.hobby();
+}
+
+ void hobby()
+{
+ String name = "Ritesh Jaswal";
+ System.out.println("NAME is - "+name);
+
+ int age = 19;
+ System.out.println("AGE is - "+age);
+}
+} 
+
+
+// QUESTION 2
+/*
+1. CLASS NAME;
+   - Ritesh
+   - Jaswal
+   
+2. FUNCTION NAME;
+   - ritesh()
+   - jaswal()
+
+3. Variable;
+   - ritesh
+   - jaswal
+
+4. Static Variable;
+   - RITESH_JASWAL
+   - ASSIGNMENT  
+*/
+
+//QUESTION 3
+/*
+1. 
+  -.java file;
+    -THIS FILE IS WHAT IS NEEDED TO BE COMPILED IN A PROGRAM.
+	-THIS IS NOT CASE SENSITIVE IN CMD CODE.
+	-javac file_name.java IS USED TO COMPILE A .java file IN CMD.
+	
+  -.class file;
+    -THIS FILE IS THE ONE WHICH GETS EXECUTED/RUN IN A PROGRAM.
+    -THIS IS CASE SENSITIVE IN CMD CODE. 	
+    -java class_name IS USED TO COMPILE A .class file IN CMD.
+    
+2. 
+  JAVA IS NOT A COMPLETE OOPS PROGRAMMING LANGUAGE AS IT NOT ONLY FOCUSES ON OBJECTS IN A PROGRAM  
+  BUT ALSO ON THE PRIMITIVE DATA TYPES LIKE INT,CHAR,BYTE,ETC. TO USE A PURE OOPS CONCEPT IN JAVA
+  WRAPPER CLASSES ARE USED OR NEEDED.
+  
+3.
+  EXAMPLE OF NR-WA:-
+  void ritzz(int a,int b)
+  {
+   int c = a+b;
+   System.out.println("VALUE OF C IS- "+c+".");
+  }   
+*/
+
+//QUESTION 4
+/*
+class CalciWorking
+{
+	public static void main(String[] args)
+    {
+	  Calci.addy();
+	  
+	  Calci.subby();
+	  
+	  Calci obj = new Calci();
+	     obj.diviji();
+
+      Calci obj1 = new Calci();
+         obj1.mull();  	  
+	}		
+}
+class Calci 
+{
+	static void addy()
+	{
+	  System.out.println("THIS IS OUTPUT OF ADDY FUNCTION.");
+	}	
+
+	static void subby()
+	{
+	System.out.println("THIS IS OUTPUT OF SUBBY FUNCTION.");	
+	}
+
+    void diviji()
+    {
+    System.out.println("THIS IS OUTPUT OF DIVIJI FUNCTION.");
+    }
+   
+    void mull()
+    {
+	System.out.println("THIS IS OUTPUT OF MULL FUNCTION.");	
+	}		
+}	
+*/
+
+//QUESTION 5
+/*
+WRAPPER CLASSES ARE A TYPE OF CLASSES NEEDED TO USE THE CONCEPT OF PURE OOPS IN JAVA AS WE KNOW THAT 
+JAVA IS NOT A COMPLETE OR PURE OOPS LANGUAGE.WRAPPER CLASSES ARE USED INSTEAD OF PRIMITIVE DATA TYPES
+LIKE int,double. THE WRAPPER CLASS OF "int" WILL BE "INTEGER" AND FOR "double" IT WILL BE "Double".
+EX:-
+    INTEGER a = 10; 
+*/
+
